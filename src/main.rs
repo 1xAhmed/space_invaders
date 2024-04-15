155a6b4c919461198de430446ee50b4c0b87f81a
1xahmed <1xahmedali@gmail.com>
2024-04-15 13:16:07 +0500

    shooting

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,10 @@ use space_invaders::frame::new_frame;
 use space_invaders::frame::Drawable;
 use space_invaders::player::Player;
 use space_invaders::render;
-use std::{error::Error, time::Duration};
+use std::{
+    error::Error,
+    time::{Duration, Instant},
+};
 use std::{io, sync::mpsc, thread};
 
 // Lets make it return a result so we can use a ? ergonomically
@@ -46,7 +49,10 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Gameloop
     let mut player = Player::new();
+    let mut instant = Instant::now();
     'gameloop: loop {
+        let delta = instant.elapsed();
+        instant = Instant::now();
         // Per-frame init
         let mut curr_frame = new_frame();
         // Input
@@ -55,6 +61,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                 match key_event.code {
                     KeyCode::Left => player.move_left(),
                     KeyCode::Right => player.move_right(),
+                    KeyCode::Char(' ') | KeyCode::Enter => {
+                        if player.shoot() {
+                            audio.play("pew");
+                        }
+                    }
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -63,6 +74,9 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+
+        // Updates
+        player.update(delta);
 
         // Draw & render
         player.draw(&mut curr_frame);
